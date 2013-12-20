b1841711037c9f2012828c304699266da02eb224
Pierre Senellart <pierre@senellart.com>
2013-12-20 15:03:19 +0100

    Fix call to basic_stats

--- webapp/views.py
+++ webapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from forms import GetDataForm, KeywordSearchForm, SERVICE_CHOICES
 from neemi.data import get_user_data, get_all_user_data
 from neemi.search import simple_keyword_search
-from neemi.stats import basic_stats
+from neemi.stats import DBAnalysis
 import time, datetime
 
 
@@ -105,7 +105,7 @@ def get_stats(request, template='stats.html'):
     print "request.method: ", request.method
 
     if request.method == 'POST':
-        return basic_stats()
+        return DBAnalysis().basic_stats()
     response = render_to_response(
             template, locals(), context_instance=RequestContext(request)
         )
